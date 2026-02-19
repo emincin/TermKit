e2c22921f4fc2de429957ad4ff90ef83a6a24b8e
emincin.macos <emincin.life@gmail.com>
2026-02-19 20:20:54 +0800

    Update Terminal.hpp

--- Source/TermKit/Terminal.hpp
+++ Source/TermKit/Terminal.hpp
@@ -1,10 +1,13 @@
 #ifndef TERMKIT_TERMINAL_HPP
 #define TERMKIT_TERMINAL_HPP
 
+#include "Common.hpp"
+
 namespace TermKit {
 
 class Terminal {
 public:
+    static function enableRawMode() -> bool;
 };
 
 } // namespace TermKit
