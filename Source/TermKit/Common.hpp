8989707e887539829be7cdbc0b1a9ea4519b8038
emincin.macos <emincin.life@gmail.com>
2026-02-20 13:47:30 +0800

    Update Common.hpp

--- Source/TermKit/Common.hpp
+++ Source/TermKit/Common.hpp
@@ -1,6 +1,8 @@
 #ifndef TERMKIT_COMMON_HPP
 #define TERMKIT_COMMON_HPP
 
+#include <cstdint>
+
 #if defined(__APPLE__) && defined(__MACH__)
 #define IS_MACOS 1
 #endif
@@ -11,5 +13,20 @@
 
 #define function auto
 #define let auto
+
+using i8 = std::int8_t;
+using u8 = std::uint8_t;
+
+using i16 = std::int16_t;
+using u16 = std::uint16_t;
+
+using i32 = std::int32_t;
+using u32 = std::uint32_t;
+
+using i64 = std::int64_t;
+using u64 = std::uint64_t;
+
+using isize = std::intptr_t;
+using usize = std::uintptr_t;
 
 #endif
