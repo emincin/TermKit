fe8f174ab229a0f723450dbe0ea9e589ccbf1eda
emincin.macos <emincin.life@gmail.com>
2026-02-22 20:14:54 +0800

    Update JSBind.hpp

--- Source/TermKit/JSBind.hpp
+++ Source/TermKit/JSBind.hpp
@@ -1,10 +1,18 @@
 #ifndef TERMKIT_JSBIND_HPP
 #define TERMKIT_JSBIND_HPP
 
+#include "quickjs.h"
+
 namespace TermKit {
 
 class JSBind {
 public:
+    JSBind();
+    ~JSBind();
+
+private:
+    JSContext* context;
+    JSRuntime* runtime;
 };
 
 } // namespace TermKit
