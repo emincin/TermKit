368b123182da36793228b1d6d41937327884cdf3
emincin.macos <emincin.life@gmail.com>
2026-02-22 20:22:04 +0800

    Update JSBind.cpp

--- Source/TermKit/JSBind.cpp
+++ Source/TermKit/JSBind.cpp
@@ -3,9 +3,13 @@
 namespace TermKit {
 
 JSBind::JSBind() {
+    runtime = JS_NewRuntime();
+    context = JS_NewContext(runtime);
 }
 
 JSBind::~JSBind() {
+    JS_FreeContext(context);
+    JS_FreeRuntime(runtime);
 }
 
 } // namespace TermKit
