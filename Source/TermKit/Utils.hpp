2de6670c7fcfb40fe449a71fb5cbab21f35895b8
emincin.macos <emincin.life@gmail.com>
2026-02-25 17:37:56 +0800

    Update Utils.hpp

--- Source/TermKit/Utils.hpp
+++ Source/TermKit/Utils.hpp
@@ -1,3 +1,15 @@
 #ifndef TERMKIT_UTILS_HPP
 #define TERMKIT_UTILS_HPP
+
+#include "Common.hpp"
+
+namespace TermKit {
+
+class Utils {
+public:
+    Utils() = delete;
+};
+
+} // namespace TermKit
+
 #endif
