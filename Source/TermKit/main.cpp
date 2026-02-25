31cd8720cedfbf7ef082ac093edc8fffd5bf24b8
emincin.macos <emincin.life@gmail.com>
2026-02-25 11:55:52 +0800

    Update main.cpp

--- Source/TermKit/main.cpp
+++ Source/TermKit/main.cpp
@@ -4,7 +4,22 @@
 
 using namespace TermKit;
 
+function testRawMode() {
+    let ok = false;
+    ok = Terminal::enableRawMode();
+    while (true) {
+    }
+    ok = Terminal::disableRawMode();
+}
+
 function parseArgs(int argc, char** argv) {
+    if (argc <= 0) {
+        return;
+    }
+    if (argc == 1) {
+    } else if (argc == 2) {
+    } else if (argc == 3) {
+    }
 }
 
 int main(int argc, char** argv) {
